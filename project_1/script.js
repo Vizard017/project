9a4c0acd6dba2f6a13e9b6b691dccd5a19cf40c6
Eugene <eugenekartashov@MacBook-Air-Evgenij.local>
2021-02-07 17:07:25 +0300

    pratic 2

--- project_1/script.js
+++ project_1/script.js
@@ -8,12 +8,33 @@ const personaMovieDB = {
      privat: false
 };
 
-const a = prompt("один из последних просмотренных филмов?", " "),
-      b = prompt("на сколько оцените его?", " "),
-      c = prompt("один из последних просмотренных филмов?", " "),
-      d = prompt("на сколько оцените его?"," ");
 
-      personaMovieDB.movies[a] = b;
-      personaMovieDB.movies[c] = d;
+
+      for (let i =0; i<2; i++){
+          const a = prompt("один из последних просмотренных филмов?", " "),
+                b = prompt("на сколько оцените его?", " ");
+
+                if (a!=null && b!=null && a !='' && b !='' && a.length<50){
+                    personaMovieDB.movies[a] = b;
+                    console.log('Done');
+                }else{
+                     console.log('error');
+                     i--;
+                }
+
+            
+      }
+
+      if (personaMovieDB.count < 10){
+           console.log('просмотрено мало фильмоэ');
+      } else if( personaMovieDB.count>= 10 && personaMovieDB.count<30 ){
+           console.log('вы класный зритель');
+      }else if (personaMovieDB.count >=30){
+            console.log('вы киноман');
+                
+           }else{
+                console.log('произошла ошибка');
+           }
+      
 
 console.log(personaMovieDB);      
